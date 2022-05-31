e1e5c8925895d9c507a2cd087960e57994c42520
Tehseen Afzal <abuashja@gmail.com>
2022-05-31 07:39:35 +0500

    Update index.js
    Add Head component to Home Page

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,10 @@ import Head from 'next/head'
 const Home = () => {
   return (
     <div>
+      <Head>
+        <title>WebDev Newz</title>
+        <meta name='keywords' content='web development, programming, coding research' />
+      </Head>
       <h1>My first Next app</h1>
     </div>
   )
