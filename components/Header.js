7dba4d253e9f006a8055507904a1565628bd4b8a
Tehseen Afzal <abuashja@gmail.com>
2022-05-31 07:38:13 +0500

    Create Header.js

---
+++ components/Header.js
@@ -0,0 +1,17 @@
+import headerStyles from "../styles/Header.module.css";
+
+const Header = () => {
+  return (
+    <div>
+      <h1 className={headerStyles.title}>
+        <span>WebDev</span>
+        Newz
+      </h1>
+      <p className={headerStyles.description}>
+        Keep up to date with the latest web dev news
+      </p>
+    </div>
+  );
+};
+
+export default Header;
